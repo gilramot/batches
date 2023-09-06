db48761fbc13539376d7b4490fba6805b2ed7c8f
AUTOMATIC PUSHER <automaticpusher@labcomputers>
2023-09-06 08:47:15 +0300

    Added file for downloads

--- push_to_github.bat
+++ push_to_github.bat
@@ -1,12 +1,6 @@
-@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
-echo A|choco install gh --force
-
 cd /D "%~dp0"
 cd ..
 
-FOR /F "delims=|" %%A IN ("%CD%") DO (
-    SET SOMEFILE=%%~nxA
-)
 
 git init
 git add .
@@ -19,12 +13,5 @@ git commit -m "init commit"
 
 gh auth login -p https -w
 
-cd ..
-gh repo create --push --private
+gh repo create --source=%CD% --private --push
 timeout 10000
-
-:file_name_from_path <resultVar> <pathVar>
-(
-    set "%~1=%~nx2"
-    exit /b
-)
